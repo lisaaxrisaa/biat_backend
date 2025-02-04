da998cdf5385a5c91e4f098d81539e200ed01e1d
Lisa Fujita <lisafujita90@gmail.com>
2025-02-03 20:12:33 -0500

    Updated register and login route for proper backend response

--- server.js
+++ server.js
@@ -9,13 +9,22 @@ app.use(
   cors({
     origin: 'http://localhost:5173',
     credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
   })
 );
+
+app.options('*', cors());
 
 app.use('/', require('./api/auth'));
 
 app.get('/', (req, res) => {
   res.send('Welcome to the backend API');
+});
+
+app.use((req, res, next) => {
+  console.log('Response Headers:', res.getHeaders()); // Log response headers
+  next();
 });
 
 app.use((err, req, res, next) => {
