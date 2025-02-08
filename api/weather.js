29e1316e430892487022a8dc0c4b3408f2747d99
Lisa Fujita <lisafujita90@gmail.com>
2025-02-07 19:27:47 -0500

    Created new GET router for itinerary

--- api/weather.js
+++ api/weather.js
@@ -6,8 +6,8 @@ const BASE_URL =
   'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/';
 
 router.get('/', async (req, res) => {
-  console.log('Request received for location:', req.query.location);
   const { location } = req.query;
+  console.log('Request received for location:', location);
 
   if (!location) {
     return res.status(400).json({ message: 'Location is required' });
@@ -19,6 +19,7 @@ router.get('/', async (req, res) => {
     const response = await axios.get(
       `${BASE_URL}${location}/today?key=${API_KEY}&unitGroup=us&elements=name,temp,feelslike,humidity,precip,precipprob,windspeed,cloudcover,sunrise,sunset,moonphase,conditions&include=current,days&contentType=json`
     );
+    console.log('API response:', response.data);
     res.status(200).json(response.data);
   } catch (error) {
     console.error('Full Error:', error);
