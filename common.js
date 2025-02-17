078a2c07e9c29352126bb2cb6da06efd8bcec12a
Nicole Riera <150852807+NicoRi5@users.noreply.github.com>
2025-02-16 19:42:57 -0500

    Added new route

--- common.js
+++ common.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
-const { faker } = require('@faker-js/faker');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-const axios = require('axios');
+const { faker } = require("@faker-js/faker");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const axios = require("axios");
 module.exports = { express, router, prisma, faker, bcrypt, jwt, axios };
